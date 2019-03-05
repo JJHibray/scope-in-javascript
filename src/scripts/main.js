a87f03d44227d5fcec24be9aaf1e889c96619e1a
Josh Hibray <jjhibray@gmail.com>
2019-03-05 13:10:58 -0600

    completed conjunction function

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+console.log("hello")
+
+/*function conjunction(firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    conjoinedWord("Master", "Card")
+    return console.log(conjunction)
+}*/
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
